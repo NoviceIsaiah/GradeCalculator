98aca50ca3239ed839c4234e948edfb0d68261cf
unknown <isaiahlopez1238@gmail.com>
2025-05-31 17:46:48 -0700

    Initial commit of Grade Calculator Project

---
+++ Grade Calculator.asm
@@ -0,0 +1,29 @@
+;CIS11
+;Isaiah Lopez
+;Final Project
+;Grade Calculator
+;Desc: The user inputs a set of 5 numerical values/integers (0-100) to
+;be calculate the max. score, min. score, and average score along with
+;letter-equivalent scores to be displayed.  
+;
+; Inputs: 
+; Outputs: 
+; Run:  Assemble the program
+;	Open the Simulate Software
+;	Load the Assembled program(.obj file)
+;	Run program
+;	Input 5 integer values (0-100)
+;	Read display for results
+
+;Program Execution----------------------------------
+
+.ORIG x3000
+
+HALT
+
+
+;SUBROUTINES----------------------------------------
+
+;DATA-----------------------------------------------
+
+.END
