a82cda76302ba17210d159bb9404989f0578af88
unknown <isaiahlopez1238@gmail.com>
2025-06-01 00:05:44 -0700

    Added beginning prompt.

--- Grade Calculator.asm
+++ Grade Calculator.asm
@@ -19,11 +19,36 @@
 
 .ORIG x3000
 
+LEA R0, startPrompt	;Loads starting prompt to user
+PUTS			;Displays prompt
+LD R6, stackBase	;Load stackBase onto R6
+
+GETVALUES
+GETC			;Get next character
+VALIDATE
+ADD R1, R0, #0		;Copy character into R1	
+STI R1, charStore
+
 HALT
 
 
 ;SUBROUTINES----------------------------------------
+PUSH
+ADD R6, R6, #1
+STR R0, R6, #0
+RET
+
+POP 
+LDR R0, R6, #0
+ADD R6, R6, #-1
+RET
+
+
 
 ;DATA-----------------------------------------------
+startPrompt	.STRINGZ "Enter 5 values 0-100: "
+stackBase	.FILL x3200
+charStore	.FILL x3050
 
+ASCtoHex 	.FILL #-30
 .END
