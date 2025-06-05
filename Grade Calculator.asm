428ecaa3d685ac5aeb6ab2bd8fd3ac0c4ba5d212
unknown <isaiahlopez1238@gmail.com>
2025-06-04 22:06:44 -0700

    Implemented user input. Looks for valid number 0-9 and only allows for 3 digitsto be inputted at a time.

--- Grade Calculator.asm
+++ Grade Calculator.asm
@@ -25,9 +25,37 @@ LD R6, stackBase	;Load stackBase onto R6
 
 GETVALUES
 GETC			;Get next character
-VALIDATE
-ADD R1, R0, #0		;Copy character into R1	
-STI R1, charStore
+OUT			;Echo character on display
+AND R1, R1, #0		;Clear R1
+ADD R1, R0, #0		;Copy R0 to R1
+ADD R1, R1, #-10	;Check if input was a newline
+BRz FINISHED		;If newline then input is finished
+
+;Input validation, checks if 0-9
+AND R1, R1, #0		;Clear R1
+ADD R1, R0, #0		;Copy R0 to R1
+LD R2, NEG39
+ADD R1, R1, R2		;Check if input is greater than 0-9
+BRp INVALID		;If so, go to invalid subroutine else continue
+
+AND R1, R1, #0		;Clear R1
+ADD R1, R0, #0		;Copy R0 to R1
+LD R2, NEG30		;R2 = -30
+ADD R1, R1, R2		;Check if input is less than 0-9 
+BRn INVALID		;If so, go to invalid subroutine else continue
+
+;Push the inputted number 0-9 on the stack
+JSR PUSH		;Else push number onto stack
+AND R1, R1, #0		;Clear R1
+AND R2, R2, #0		;Clear R2
+ADD R3, R3, #-1		;Adds one to counter
+LD R2, MAXCOUNT		;Loads max number count (3) since values can't go past 100
+ADD R2, R2, R3		;Subtract counter from maxcount		 
+BRnz FINISHED		;Once 3 numbers 0-9 are inputted, grade value is inputed
+BR GETVALUES
+
+FINISHED
+HALT
 
 HALT
 
@@ -43,12 +71,25 @@ LDR R0, R6, #0
 ADD R6, R6, #-1
 RET
 
+INVALID
+LD R0, NEWLINE
+OUT
+LEA R0, invalidPrompt
+PUTS
+BR GETVALUES
 
+;STRINGZ-----------------------------------------------
+startPrompt	.STRINGZ "Enter 5 values 0-100: \n"
+invalidPrompt	.STRINGZ "Input was invalid. Try entering a number 0-9 again.\n"
 
-;DATA-----------------------------------------------
-startPrompt	.STRINGZ "Enter 5 values 0-100: "
+;Address locations-------------------------------------
 stackBase	.FILL x3200
 charStore	.FILL x3050
 
+;Filled values
+NEG39 		.FILL xFFC7
+NEG30		.FILL xFFD0
+NEWLINE		.FILL x000A
+MAXCOUNT 	.FILL x0003
 ASCtoHex 	.FILL #-30
 .END
